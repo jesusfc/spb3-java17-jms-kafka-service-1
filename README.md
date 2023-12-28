a8e19a127cefcf57e9bad4aa4103778375d3440b
Jesusfc <jfcaraballo@gmail.com>
2023-12-28 16:42:28 +0100

    First commit, lets start

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spb3-java17-jms-kafka
 Project to test JMS Messaging and Apache Kafka with Spring Boot 3 and Java 17
+- Project Lombok
+- JMS Messaging
+- Apache Kafka
---
+++ src/main/java/com/jesusfc/demo/Spb3Java17JmsKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.jesusfc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spb3Java17JmsKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spb3Java17JmsKafkaApplication.class, args);
+	}
+
+}
