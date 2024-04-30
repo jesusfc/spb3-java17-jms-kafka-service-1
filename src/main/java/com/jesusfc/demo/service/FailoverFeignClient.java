8601c0a7aa218c51074bb9df8f868a417ab17671
Jesusfc <jfcaraballo@gmail.com>
2024-04-30 16:06:49 +0200

    Failover feign client

---
+++ src/main/java/com/jesusfc/demo/service/FailoverFeignClient.java
@@ -0,0 +1,9 @@
+package com.jesusfc.demo.service;
+
+/**
+ * Author Jesús Fdez. Caraballo
+ * jesus.fdez.caraballo@gmail.com
+ * Created on abr - 2024
+ */
+public interface FailoverFeignClient {
+}
