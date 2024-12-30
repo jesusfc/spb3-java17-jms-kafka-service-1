eba16f5117cc5fed181025784f307d727391ab78
Jesusfc <jfcaraballo@gmail.com>
2024-12-30 13:06:18 +0100

    update Security for Feign Service

--- src/main/java/com/jesusfc/demo/config/FeignClientsConfig.java
+++ src/main/java/com/jesusfc/demo/config/FeignClientsConfig.java
@@ -1,6 +1,8 @@
 package com.jesusfc.demo.config;
 
 import feign.Logger;
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +13,17 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class FeignClientsConfig {
+
+    @Value("${feign.client-service-2.username}")
+    private String username;
+
+    @Value("${feign.client-service-2.password}")
+    private String password;
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor(username, password);
+    }
 
     @Bean
     Logger.Level feignLoggerLevel() {
